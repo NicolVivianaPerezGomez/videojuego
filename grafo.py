4cf13460e9faad911a5a61e877ed08087445266c
Julian Bejarano <jbejarano@uniempresarial.edu.co>
2024-12-03 17:57:56 -0500

    Correcion

--- grafo.py
+++ grafo.py
@@ -2,16 +2,5 @@ import mysql.connector
 conexion = mysql.connector.connect(host="localhost", user="root" password="1234", database="videojuego")
 cursor = conexion.cursor()
 def conectar():
-<<<<<<< HEAD
 
- #Hola Grupo 2  
- #Hola Juli y Saris
-
-    print("Conectado a la base de datos")
-
-#HOLAAAAAAAAAAAAAAAAAAAAAAAAAA
-#Hollllaaaa
-#eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
-=======
-    print("Conectado a la base de datos")
->>>>>>> c7a5eac0287a078a79b4658b768dbeed58ea3323
+# Corección de errores
