c7a5eac0287a078a79b4658b768dbeed58ea3323
NicolVivianaPerezGomez <nperez@uniempresarial.edu.co>
2024-12-03 17:53:35 -0500

    new

--- grafo.py
+++ grafo.py
@@ -1,16 +1,5 @@
 import mysql.connector
-
-#PARTE ARTE
 conexion = mysql.connector.connect(host="localhost", user="root" password="1234", database="videojuego")
 cursor = conexion.cursor()
-
 def conectar():
-
- #Hola Grupo 2  
- #Hola Juli y Saris
-
     print("Conectado a la base de datos")
-
-#HOLAAAAAAAAAAAAAAAAAAAAAAAAAA
-#Hollllaaaa
-#porfin
