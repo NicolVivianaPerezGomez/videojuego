a0d7ad28b249c1e10fc37e37915b341b632a5d72
NicolVivianaPerezGomez <nperez@uniempresarial.edu.co>
2024-12-06 21:39:16 -0500

    new

--- crud.py
+++ crud.py
@@ -1,16 +1,19 @@
 import mysql.connector
+import json
 
 # CONECTAMOS LA BASE DE DATOS
-db=mysql.connector.connect(user='root',password='1234',host='localhost',database="videojuego", auth_plugin="myql_native_password")
+db = mysql.connector.connect(user='root',password='1234',host='localhost',database="videojuego", auth_plugin="mysql_native_password")
 cursor = db.cursor()
 
 # CRUD
-# Regustar a un jugador
+import json
 def RegistraJugador(jug_nombre, nivel, puntuacion, equipo, inventario):
-    cursor = db.cursor()
-    cursor.callproc('RegistraJugador',(jug_nombre,nivel,puntuacion,equipo,inventario))
+    inventario_json = json.dumps(inventario)  # Convertir el diccionario a JSON
+    cursor.callproc('RegistraJugador', (jug_nombre, nivel, puntuacion, equipo, inventario_json))
+
+# Uso de la función
+    RegistraJugador('Sandra', 3, 150, 'Equipo1', {"Espada":1, "Arco": 4})
     db.commit()
+    print("Jugador registrado exitosamente.")
     cursor.close()
     db.close()
-    print(f'Jugador Registrado{jug_nombre}{nivel}{puntuacion}{equipo}{nivel}.')
-    
