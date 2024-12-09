968d0313f82cec0e13a0a20cfcecd58c4c02b639
NicolVivianaPerezGomez <nperez@uniempresarial.edu.co>
2024-12-08 20:05:12 -0500

    new

--- Backend/Mundos.py
+++ Backend/Mundos.py
@@ -1,4 +1,10 @@
 #GRAFO
+import mysql.connector
+import json
+
+# CONECTAMOS LA BASE DE DATOS
+db = mysql.connector.connect(user='root',password='1234',host='localhost',database="videojuego", auth_plugin="mysql_native_password")
+cursor = db.cursor()
 class Graph:
     def __init__(self):
         self.graph = {}
